6e3f676ab7b1488018eaf785b0de58104cf79caa
GEORGIA <georgia.christodoulou@gmail.com>
2025-06-07 18:57:17 +0200

    new tests

--- yolo/src/yolo_package/src/prediction.py
+++ yolo/src/yolo_package/src/prediction.py
@@ -7,14 +7,14 @@ model = YOLO("C:/Users/user/Documents/TUe/Q4/5LIA0 Embedded visual control/Works
 
 # WEBCAM
 # accepts all formats - image/dir/Path/URL/video/PIL/ndarray. 0 for webcam
-#results = model.predict(source="0")
+results = model.predict(source="0", show=True)
 
 # IMAGES FOLDER
 test_folder = "C:/Users/user/Documents/TUe/Q4/5LIA0 Embedded visual control/Workshops/EVC/workshops/yolo_ws/yolo/src/yolo_package/src/archive/car/test/images"
 #results = model.predict(source=test_folder, save=True)  # Display preds. Accepts all YOLO predict arguments
 
 # video feed
-results = model.predict(source="C:/Users/user/Documents/TUe/Q4/5LIA0 Embedded visual control/Workshops/EVC/workshops/yolo_ws/yolo/src/yolo_package/src/archive/video_inf.mp4", show=True)
+#results = model.predict(source="C:/Users/user/Documents/TUe/Q4/5LIA0 Embedded visual control/Workshops/EVC/workshops/yolo_ws/yolo/src/yolo_package/src/archive/video_inf.mp4", show=True)
 #results = model.predict(source="video_inf.mp4", show=True)
 
 # test on single image:
@@ -23,8 +23,8 @@ results = model.predict(source="C:/Users/user/Documents/TUe/Q4/5LIA0 Embedded vi
 #results = model.predict(source=im1, save=True)  # save plotted images
 
 # from ndarray
-#im2 = cv2.imread("bus.jpg")
-#results = model.predict(source=im2, save=True, save_txt=True)  # save predictions as labels
+im2 = cv2.imread("speed30_02.png")
+#results = model.predict(source=im2, show=True)  # save predictions as labels
 
 # from list of PIL/ndarray
 #results = model.predict(source=[im1, im2])
