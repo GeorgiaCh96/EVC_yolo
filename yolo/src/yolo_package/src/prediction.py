3b1508c730891d001f39e1d149b3ede7477b3c13
GEORGIA <georgia.christodoulou@gmail.com>
2025-06-08 13:05:30 +0200

    chnages on processing node

--- yolo/src/yolo_package/src/prediction.py
+++ yolo/src/yolo_package/src/prediction.py
@@ -3,11 +3,29 @@ from PIL import Image
 
 from ultralytics import YOLO
 
+class_names = {
+            0: "Green_light",
+            1: "Red_light",
+            2: "speed_limit_10",
+            3: "speed_limit_100",
+            4: "speed_limit_110" ,
+            5: "speed_limit_120",
+            6: "speed_limit_20",
+            7: "speed_limit_30",
+            8: "speed_limit_40",
+            9: "speed_limit_50" ,
+            10: "speed_limit_60",
+            11: "speed_limit_70",
+            12: "speed_limit_80",
+            13: "speed_limit_90",
+            14: "stop" 
+}
+
 model = YOLO("C:/Users/user/Documents/TUe/Q4/5LIA0 Embedded visual control/Workshops/EVC/workshops/yolo_ws/yolo/src/yolo_package/src/runs/detect/yolov8n_custom6/weights/best.pt")
 
 # WEBCAM
 # accepts all formats - image/dir/Path/URL/video/PIL/ndarray. 0 for webcam
-results = model.predict(source="0", show=True)
+#results = model.predict(source="0", show=True)
 
 # IMAGES FOLDER
 test_folder = "C:/Users/user/Documents/TUe/Q4/5LIA0 Embedded visual control/Workshops/EVC/workshops/yolo_ws/yolo/src/yolo_package/src/archive/car/test/images"
@@ -24,7 +42,17 @@ test_folder = "C:/Users/user/Documents/TUe/Q4/5LIA0 Embedded visual control/Work
 
 # from ndarray
 im2 = cv2.imread("speed30_02.png")
-#results = model.predict(source=im2, show=True)  # save predictions as labels
+results = model.predict(source=im2, show=True)  # save predictions as labels
+results = results[0]
+print("results: ", results[0])
+boxes   = results.boxes.xyxy.cpu().numpy() 
+print("boxes: ", boxes)
+scores  = results.boxes.conf.cpu().numpy()  
+print("scores: ", scores)
+classes = results.boxes.cls.cpu().numpy()    
+print("classes: " , int(classes[0]))
+print("class name: ", class_names[int(classes[0])])
+
 
 # from list of PIL/ndarray
 #results = model.predict(source=[im1, im2])
--- yolo/src/yolo_package/src/yolov8_det.py
+++ yolo/src/yolo_package/src/yolov8_det.py
@@ -1,51 +1,136 @@
 #!/usr/bin/env python3
 
+import os
 import rospy, torch, cv2
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
-from std_msgs.msg import String
+from std_msgs.msg import String, CompressedImage
 from ultralytics import YOLO 
 
 
 class Yolov8Detector:
     def __init__(self):
+        self.initialized=False
+        rospy.loginfo("Initializing YOLO detector node...")
+        rospy.init_node("yolov8_detector", anonymous=True, xmlrpc_port=49100, tcpros_port=49101)  # anonymous=True?
+
         self.bridge   = CvBridge()
-        #self.pub      = rospy.Publisher("/camera/overlay", Image, queue_size=10)
-        self.object_pub = rospy.Publisher("/motion/yolo_motion_command", String, queue_size=1)
+        self.first_image_received = False
+
+        # Subscriber for undistorted images (sent from Calibration_node)
+        self.sub_image = rospy.Subscriber(
+            "/camera/img_undistorted", 
+            CompressedImage,
+            self.image_cb,
+            buff_size=2**24,
+            queue_size=1)  # 1 or None?
+        
+        # publish motion command to /motion/yolo_motion_command topic
+        self.command_pub = rospy.Publisher(
+            "/motion/yolo_motion_command", 
+            String, 
+            queue_size=1)
+        
+        # publish annotated image (for inspection)
+        self.annotated_pub = rospy.Publisher(
+            "/camera/yolo_processed_image",
+            CompressedImage,
+            queue_size=1
+        )
+        
+        self.initialized=True
+        rospy.loginfo("YOLO detector node initialized!")
+
         self.device   = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-        self.model = YOLO('/home/ubuntu/EVC/workshops/workshop4_motion_1654411/best.pt').to(self.device)
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        try:
+            self.model = YOLO(os.path.join(current_dir, "best.pt")).to(self.device)
+            rospy.loginfo("YOLO Model loaded on %s", self.device)
+        except Exception as e:
+            rospy.logerr("Could not load YOLO model: {}".format(e))
+            self.model=None
 
-        self.model.eval()
-        rospy.loginfo("YOLO Model loaded on %s", self.device)
 
-        rospy.Subscriber("/camera/image_undistorted", Image, self.cb, queue_size=None)
+    def image_cb(self, msg):
+        """
+            Callback fuction to process incomming image messages
+        """
+        rospy.loginfo("entered into image callback")
 
-    def cb(self, msg):
-        frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-        results = self.model(frame)[0]
-        boxes   = results.boxes.xyxy.cpu().numpy() 
-        scores  = results.boxes.conf.cpu().numpy()  
-        classes = results.boxes.cls.cpu().numpy()    
+        class_names = {
+            0: "Green_light",
+            1: "Red_light",
+            2: "speed_limit_10",
+            3: "speed_limit_100",
+            4: "speed_limit_110" ,
+            5: "speed_limit_120",
+            6: "speed_limit_20",
+            7: "speed_limit_30",
+            8: "speed_limit_40",
+            9: "speed_limit_50" ,
+            10: "speed_limit_60",
+            11: "speed_limit_70",
+            12: "speed_limit_80",
+            13: "speed_limit_90",
+            14: "stop" 
+        }
 
-        for (x1, y1, x2, y2), cls_id, conf in zip(boxes, classes, scores):
-            label_txt = f"{self.model.names[int(cls_id)]} {conf*100:.0f}%"
+        if not self.initialized:
+            return
 
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)),
-                          (0, 255, 0), 2)
-            cv2.putText(frame, label_txt, (int(x1), int(y1)-10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
+        if not self.first_image_received:
+            self.first_image_received = True
+            rospy.loginfo("YOLO subscriber captured first image from publisher")
 
-            # publish just the class name (without %) on /detected_object
-            label_msg = String()
-            label_msg.data = self.model.names[int(cls_id)]
-            self.object_pub.publish(label_msg)
+
+        try:
+            frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+            rospy.loginfo("PubSub delay: {}".format((rospy.Time.now() - msg.header.stamp).to_sec()))
+
+            #results = self.model(frame)[0]
+            results = self.model.predict(source=frame)
+            results = results[0]
+            boxes   = results.boxes.xyxy.cpu().numpy() 
+            scores  = results.boxes.conf.cpu().numpy()  
+            classes = results.boxes.cls.cpu().numpy()  
+            rospy.loginfo("Predicted object: {}".format(class_names[int(classes[0])])) 
+
+            for (x1, y1, x2, y2), cls_id, conf in zip(boxes, classes, scores):
+                label_txt = f"{self.model.names[int(cls_id)]} {conf*100:.0f}%"
+
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)),
+                            (0, 255, 0), 2)
+                cv2.putText(frame, label_txt, (int(x1), int(y1)-10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
+
+                # publish just the class name (without %) on /detected_object
+                #label_msg = String()
+                #label_msg.data = self.model.names[int(cls_id)]
+                #self.object_pub.publish(label_msg)
+                
+
+                # print the motion command
+                if int(cls_id) in range(2, 14):
+                    rospy.loginfo("Send slow down motion command")
+                elif int(cls_id) in [1, 14]:
+                    rospy.loginfo("Send stop command")
+
+
+
+        except Exception as e:
+            rospy.logerr("Error processing image: {}".format(e))
+
 
         # out = self.bridge.cv2_to_imgmsg(frame, encoding="bgr8")
         # out.header = msg.header                
         # self.pub.publish(out)
 
 if __name__ == "__main__":
-    rospy.init_node("yolov8_detector", xmlrpc_port=49100, tcpros_port=49101)
-    Yolov8Detector()
-    rospy.spin()
+    try:
+        Yolov8Detector()
+        rate=rospy.Rate(10)
+        # Keep the node running
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
