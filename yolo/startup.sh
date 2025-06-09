549a63266182bc1a786e7123241bbeb012cfe0fe
GEORGIA <georgia.christodoulou@gmail.com>
2025-06-09 21:08:17 +0200

    modified order of commands

--- yolo/startup.sh
+++ yolo/startup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+
+# Step 5: Export ROS environment variables
+export ROS_MASTER_URI=http://192.168.8.2:11311
+export ROS_IP=192.168.8.248
+export DISPLAY=192.168.8.248:0
+
+
 # Step 1: Clean previous build (optional)
 rm -rf build/ devel/
+
 
 # Step 2: Source ROS
 source /opt/ros/noetic/setup.bash
@@ -12,10 +20,7 @@ catkin_make
 # Step 4: Source the workspace
 source devel/setup.bash
 
-# Step 5: Export ROS environment variables
-export ROS_MASTER_URI=http://192.168.8.2:11311
-export ROS_IP=192.168.8.248
-export DISPLAY=192.168.8.248:0
+
 
 # Step 6: Add PYTHONPATH to .bashrc if not already added
 grep -qxF 'export PYTHONPATH=$PYTHONPATH:/home/ubuntu/yolo/devel/lib/python3.8/dist-packages' ~/.bashrc || \
